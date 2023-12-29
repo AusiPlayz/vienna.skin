d351348b73deb7183e22a28e635a2731831de27c
AustrianNoah <151822733+AustrianNoah@users.noreply.github.com>
2023-12-29 03:21:03 +0100

    Update list.js

--- list.js
+++ list.js
@@ -2,7 +2,7 @@ var flare = require("cloudflare")
 var cf = flare({
   token: process.env.CF_TOKEN,
 })
-cf.dnsRecords.browse("2bf779292ec80723b8b7a94bb651ea7d").then((records) => {
+cf.dnsRecords.browse("ee3044599a63e7a5b78d0da8855c7d2b").then((records) => {
   const availabilityFilter = records.result.filter((record) => {
     return record.comment == process.env.EVENT_USER_LOGIN
   })
